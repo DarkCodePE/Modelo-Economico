2108371cb7f0133cf8bc78bd360f3857f069f4ef
Orlando Kuan BecerrA <orlandokuanb@gmail.com>
2024-01-16 01:22:19 -0500

    REFACTOR: add new concepts to colombia -rev2

---
+++ src/main/java/ms/hispam/budget/rules/SalaryCalculationStrategyImpl.java
@@ -0,0 +1,12 @@
+package ms.hispam.budget.rules;
+
+import ms.hispam.budget.dto.ParametersDTO;
+
+import java.util.List;
+
+public class SalaryCalculationStrategyImpl implements SalaryCalculationStrategy{
+    @Override
+    public double calculateSalary(List<ParametersDTO> parameters, String period) {
+        return 0;
+    }
+}
