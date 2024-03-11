ff833941bb4a20530c2804d0ec26a4b7d2af0619
Gherson Perez <ghersonperez@MacBook-Pro-de-Gherson.local>
2024-03-11 12:05:48 -0500

    cambios

--- src/main/java/ms/hispam/budget/util/MailFeignClient.java
+++ src/main/java/ms/hispam/budget/util/MailFeignClient.java
@@ -4,10 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "MAIL", url = "${mail}")
 public interface MailFeignClient {
     @PostMapping(value = "/apimail", consumes = MediaType.APPLICATION_JSON_VALUE)
-    boolean sendEmail(@RequestBody SendMail sendMail);
+    boolean sendEmail(@RequestBody SendMail sendMail,@RequestHeader(value = ("Ocp-Apim-Subscription-Key")) String subscription);
 }
 
