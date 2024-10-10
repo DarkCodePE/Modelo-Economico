420bf3ca91ffb80b5deb04e53a9a8f92223e8f80
Orlando <orlandokuanb@gmail.com>
2024-10-10 01:24:24 -0500

    FIX ERROR DISABLED POSITION SERIALIZATION

--- src/main/java/ms/hispam/budget/dto/PositionBaselineDTO.java
+++ src/main/java/ms/hispam/budget/dto/PositionBaselineDTO.java
@@ -1,5 +1,6 @@
 package ms.hispam.budget.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Getter
@@ -7,6 +8,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PositionBaselineDTO {
 
     private String position;
